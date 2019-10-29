4e964c3225074f078d9471c33063168e07b6cf8f
ddgksf2013 <37397537+ddgksf2013@users.noreply.github.com>
2019-10-29 20:31:50 +0800

    Update hulushike.js

--- Script/hulushike.js
+++ Script/hulushike.js
@@ -4,27 +4,18 @@ const path1 = "api";
 if (url.indexOf(path1) != -1) 
 {
 	let obj = JSON.parse(body);
-	//if(obj.containsKey("result"))
-	//{
-		obj.result.isFree = "1";
-  		obj.result.isBuy = "1";
-		obj.result.userRight = "1";
-		//if(obj.result.containsKey("user"))
-		//{
-			obj.result.user.isBuy = "1";
-			obj.result.user.userRight = "1";
-		//}
-		//if(obj.result.containsKey("resource"))
-		//{
-			for (i in obj.result.resource.items) 
-			{
-				for (j in obj.result.resource.items[i].articles)
-				{
-				obj.result.resource.items[i].articles[j].isFree = "1";
-				}		
-			}
-		//}
-	//}
+	for (i in obj.result.resource.items) 
+	{
+		for (j in obj.result.resource.items[i].articles)
+		{
+			obj.result.resource.items[i].articles[j].isFree = "1";
+		}		
+	}
+	obj.result.isFree = "1";
+  	obj.result.isBuy = "1";
+	obj.result.userRight = "1";
+	obj.result.user.isBuy = "1";
+	obj.result.user.userRight = "1";
 	body = JSON.stringify(obj);
  }
 $done({body});
