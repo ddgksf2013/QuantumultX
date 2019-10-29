0681db6c2395ccde090fc6a023e9be1ebc025a80
ddgksf2013 <37397537+ddgksf2013@users.noreply.github.com>
2019-10-29 17:49:41 +0800

    Update hulushike.js

--- Script/hulushike.js
+++ Script/hulushike.js
@@ -3,11 +3,16 @@ var url = $request.url;
 const path1 = "api";
 if (url.indexOf(path1) != -1) {
 	let obj = JSON.parse(body);
-	for (i in obj.data) {
-	obj.result.isFree = "1";
-  obj.result.isBuy = "1";
-  obj.result.userRight = "1";
+	for (i in obj.result.resource.items) {
+	for (j in obj.result.resource.items[i].articles){
+	obj.result.resource.items[i].articles[j].isFree = "1";
+	}	
 	}
+	obj.result.isFree = "1";
+  	obj.result.isBuy = "1";
+	obj.result.user.isBuy = "1";
+	obj.result.user.userRight = "1";
+  	obj.result.userRight = "1";
 	body = JSON.stringify(obj);
  }
 $done({body});
